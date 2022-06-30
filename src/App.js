f0bdc533516e880ac352c2b304889db96c6d0708
ahmetaric <ahmet.aric1965@gmail.com>
2022-06-30 12:55:38 +0300

    item

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Card from './components/card/Card';
 import Header from "./components/header/Header";
 
 
@@ -6,8 +7,9 @@ import Header from "./components/header/Header";
 function App() {
   return (
     <div className="main-container">
-      <h1>Language Cards Project</h1>
+     
       <Header/>
+      <Card/>
     </div>
   );
 }
--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,1 +1,22 @@
+import "./Card.css";
+import {languages} from "../../helpers/data";
+import Item from "../item/Item";
 
+const Card = () => {
+    console.log(languages);
+  return (
+    <div className="cards-area-container">
+        <div className="bars"></div>
+        <h1 className="language-title">Languages</h1>
+        <div className="cards-container">
+            {
+                languages.map((item,index)=>{
+                 return <Item card = {item} />;
+                })
+            }
+        </div>
+    </div>
+  )
+};
+
+export default Card;
