dfeeb9f5334b2f6af78fad4d4ab9f25f0334e3c4
ahmetaric <ahmet.aric1965@gmail.com>
2022-06-30 12:01:21 +0300

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Header from "./components/header/Header";
+
 
 
 function App() {
   return (
     <div className="main-container">
-     
+      <h1>Language Cards Project</h1>
+      <Header/>
     </div>
   );
 }
